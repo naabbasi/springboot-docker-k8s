016b6385dbc221074a7782f3aa20353a3bea66bc
Noman Ali <noman.ali.abbasy@gmail.com>
2024-10-19 15:53:21 +0300

    k3d with golang server (Demo)

--- Kubernetes-k3d/project/build.sh
+++ Kubernetes-k3d/project/build.sh
@@ -1,13 +1,15 @@
 #!/usr/bin/env sh
 
-docker rmi go-web-server && docker rmi k3d-registry.localhost:5050/go-web-server:v1.0
+docker rmi go-web-server:latest && docker rmi k3d-registry.localhost:5050/go-web-server:v1.0
 
-docker build . -t k3d-registry.localhost:5050/go-web-server:v1.0
+docker build . -t go-web-server:latest
 
 # Setup k3d
 docker tag go-web-server:latest k3d-registry.localhost:5050/go-web-server:v1.0
 
 docker push k3d-registry.localhost:5050/go-web-server:v1.0
+
+kubectl delete deployment go-web-server && kubectl delete svc go-web-server
 
 #If pod status is in PENDING, run the following or cleanup on the disk to make disk space available
 # kubectl get events
@@ -15,17 +17,17 @@ docker push k3d-registry.localhost:5050/go-web-server:v1.0
 # systemctl restart containerd && systemctl restart kubelet
 
 #kubectl run go-web-server --image=k3d-registry.localhost:5050/go-web-server:v1.0
-#kubectl create deployment go-web-server --image=k3d-registry.localhost:5050/go-web-server:v1.0
+kubectl create deployment go-web-server --image=k3d-registry.localhost:5050/go-web-server:v1.0
 # Cluster IP
 #kubectl create service clusterip go-web-server --tcp=9091:9091
   #clusterip working fine by using port forward
 #kubectl expose deployment go-web-server --port=9091 --type=LoadBalancer
   #access via external ip
 # NodePort range = 30000-32767
-#kubectl create service nodeport go-web-server --tcp=9091:9091 --node-port=30001
+kubectl create service nodeport go-web-server --tcp=9091:9091 --node-port=30001
 # LoadBalancer
-kubectl create service loadbalancer go-web-server --tcp=9091:9091
+#kubectl create service loadbalancer go-web-server --tcp=9091:9091
 
-kubectl apply -f ./deployment.yaml
-kubectl apply -f ./service-node-port.yaml
+#kubectl apply -f ./deployment.yaml
+#kubectl apply -f ./service-node-port.yaml
 #kubectl apply -f ./ingress.yaml
