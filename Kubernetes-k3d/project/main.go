015ec722a79415d4373d0ae76772eaa734f51902
Noman Ali <noman.ali.abbasy@gmail.com>
2024-10-19 14:46:06 +0300

    k3d with golang server (Demo)

--- Kubernetes-k3d/project/main.go
+++ Kubernetes-k3d/project/main.go
@@ -2,11 +2,33 @@ package main
 
 // Import Packages
 import (
+    "html/template"
     "log"
     "net/http"
+    "os"
 )
 
+type PageData struct {
+    Hostname string
+}
+
 func main() {
+    hostname, err := os.Hostname()
+    if err != nil {
+        log.Println("Error:", err)
+        return
+    }
+
+    log.Println("Hostname:", hostname)
+
+    tmpl := template.Must(template.ParseFiles("./content/about.html"))
+    http.HandleFunc("/about", func(w http.ResponseWriter, r *http.Request) {
+        data := PageData{
+            Hostname: hostname,
+        }
+
+        tmpl.Execute(w, data)
+    })
 
     // Server the Desired HTML File
     http.Handle("/", http.FileServer(http.Dir("./content")))
